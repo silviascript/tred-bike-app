6602d7a2510ace4f6c50259375e5023f78c1154f
Christine Lovett <clovett07@gmail.com>
2015-09-17 14:34:13 -0400

    devise removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
   has_many :rides
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: 'application#index'
     resources :users do
       resources :rides
